d43f8c9ca804c53d8ecf0b47cb7017af6252ec10
Peter Ivanov <ivanovp@gmail.com>
2017-12-31 17:39:04 +0100

    Z homing was enabled as endstop switch was installed.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -474,7 +474,7 @@
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
 #define USE_XMIN_PLUG
 #define USE_YMIN_PLUG
-//#define USE_ZMIN_PLUG
+#define USE_ZMIN_PLUG
 //#define USE_XMAX_PLUG
 //#define USE_YMAX_PLUG
 //#define USE_ZMAX_PLUG
@@ -777,7 +777,7 @@
 // :[-1,1]
 #define X_HOME_DIR -1
 #define Y_HOME_DIR -1
-#define Z_HOME_DIR 0
+#define Z_HOME_DIR -1
 
 // @section machine
 
