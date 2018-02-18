38157d2ec229a9b3b1dfc97d1d54d63ed1d53e9a
Peter Ivanov <ivanovp@gmail.com>
2018-02-18 15:30:06 +0100

    16 microsteps are used instead of 32.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -531,7 +531,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 8000, 428 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 214 }
 
 /**
  * Default Max Feed Rate (mm/s)
